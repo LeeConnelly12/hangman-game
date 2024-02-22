f65a096198abde92d10d1a39c946f8167e77d536
Lee Connelly <lee@leeconnelly.dev>
2024-02-22 20:52:18 +0000

    Use Mouse Memoirs font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Mouse Memoirs", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         "dark-navy": "#261676",
         blue: "#2463FF",
