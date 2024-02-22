6d88f658fda9759aea700a1ac17be1829df3ec17
Lee Connelly <lee@leeconnelly.dev>
2024-02-22 20:49:24 +0000

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-navy": "#261676",
+        blue: "#2463FF",
+      },
+    },
   },
   plugins: [],
-};
+}
